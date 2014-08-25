759d386827a5c474f8bbd687f765808ff62d438d
Thomas Flyvholm <tf@yscan.it>
2014-08-25 17:28:40 +0200

    fixed upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,7 +1,12 @@
+// Upload.js for DOE Site
+// Thomas Flyvholm, 2014
+// TODO: @optimization: Start over from scratch?
+// HELPS STYLING THE CUSTOM UPLOAD BUTTON
 jQuery(document).ready(function($){
 	$('#file-url').bind("click" , function () {
         $('#file-picker').click();
     });
+// ASSIGN PICKER-VALUE TO PLACEHOLDER-ATTRIBUTE IN URL
     document.getElementById("file-picker").onchange = function () {
     	document.getElementById("file-url").value = this.value;
 	};
